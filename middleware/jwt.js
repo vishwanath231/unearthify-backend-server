2cc04e47e75e3dc8f351fd0a5b76522821dadaeb
blisstech214 <blisstech214@gmail.com>
2025-01-09 17:50:19 +0530

    updated

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,33 +1,4 @@
-// const jwt = require("jsonwebtoken");
-// const Registration = require("../model/registration");
-// const secretKey="familyConnect"
-// const authenticateUser = async (req, res, next) => {
-//   const token =
-//     req.headers.authorization && req.headers.authorization.split(" ")[1]; // Extract the token
-//   if (!token) return res.status(403).json({ message: "Token is required" });
-//   console.log(" get token ", token);
 
-//   try {
-//     const decoded = jwt.verify(token, secretKey); // Verify the token
-//     console.log("decoded", decoded);
-
-//     const user = await Registration.findById(decoded.id);
-
-//     console.log("user detail", user);
-
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-
-//     req.user = user; // Attach user data to the request
-//     next();
-//   } catch (error) {
-//     console.error("Token verification error:", error.message); // Log error for debugging
-//     return res.status(403).json({ message: "Invalid or expired token" });
-//   }
-// };
-
-// module.exports = authenticateUser;
 const jwt = require("jsonwebtoken");
 const Registration = require("../model/registration");
 
