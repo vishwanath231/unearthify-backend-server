6f1191f731ecc1d66029eda49bf0274787e59f04
Your Name <your-email@example.com>
2026-01-13 11:42:56 +0530

    code optimized

--- controller/artformController.js
+++
@@ -1,7 +0,0 @@
-// This controller is deprecated and has been replaced by:
-// - controller/artform/artCategoryController.js
-// - controller/artform/artDetailController.js
-//
-// Please check router/artFormRoutes.js for new routes.
-
-module.exports = {};
